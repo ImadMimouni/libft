a57c9f85b58c01047bca0e49902b18289abc3257
ImadMimouni <81130422+ImadMimouni@users.noreply.github.com>
2022-10-23 21:10:10 +0100

    1

---
+++ ft_strjoin.c
@@ -0,0 +1,48 @@
+#include "libft.h"
+
+int	ft_strlen(const char *s)
+{
+	int len;
+
+	len = 0;
+	if (!s)
+		return (len);
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	unsigned int	len_s1;
+	unsigned int	len_s2;
+	unsigned int	i;
+	unsigned int	j;
+	char			*out_str;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	i = 0;
+	j = -1;
+	if (!(out_str = malloc(sizeof(char) * (len_s1 + len_s2 + 1))))
+		return (NULL);
+	while (s1[i])
+	{
+		out_str[i] = s1[i];
+		i++;
+	}
+	while (s2[++j])
+		out_str[i++] = s2[j];
+	out_str[i] = '\0';
+	return (out_str);
+}
+
+// int main ()
+// {
+// 	char s[]="1111 knkn";
+// 	char ss[]="imad";
+
+// 	printf("%s",ft_strjoin(ss,s));
+// }
